e8b0ed22f95844e875fb7c9c3afc1611256297ee
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-03-20 16:07:27 +0300

    add testing component

---
+++ src/stories/Test.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import Test from "../components/Test"
+
+const meta = {
+  title: "Example/Test",
+  component: Test,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  args: {
+    title: "Test",
+  },
+} satisfies Meta<typeof Test>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    title: "new Test",
+  },
+}
