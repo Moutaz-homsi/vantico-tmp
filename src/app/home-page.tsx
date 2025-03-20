32ae37c123504b412d23679d258a840dafcd082a
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-03-20 18:29:28 +0300

    update project layout

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -1,13 +1,18 @@
+import JsonPreview from "@/components/JsonPreview"
+import Container from "@/components/layout/container"
+import Typography from "@/components/ui/typography"
 import Link from "next/link"
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Home page</h1>
-        <Link href="/products">View products</Link>
-      </main>
+    <div>
+      <Container className="py-8">
+        <Typography variant="h1">Home page</Typography>
+        <Link href="/products">
+          <Typography variant="h3">View products</Typography>
+        </Link>
+        <JsonPreview title={"json data example"} object={{ example: "data" }} />
+      </Container>
     </div>
   )
 }
