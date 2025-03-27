475cea49fe03b884bfaeeeff8311afadc708a079
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-03-27 09:00:48 +0300

    update theme

--- src/components/layout/container.jsx
+++ src/components/layout/container.jsx
@@ -2,5 +2,5 @@ import { cn } from "@/utils/style-utils"
 import React from "react"
 
 export default function Container({ children, className = "" }) {
-  return <div className={cn("container mx-auto", className)}>{children}</div>
+  return <div className={cn("container mx-auto md:max-w-5xl", className)}>{children}</div>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
+// ⚠️WARN: if you need to customize the tailwindcss configuration, use global.css instead of this file
+// currently we need this just for storybook
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./stories/**/*.{js,ts,jsx,tsx}", // Here!
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./src/app/**/*.{js,ts,jsx,tsx}",
+		"./stories/**/*.{js,ts,jsx,tsx}" // Here!
+	],
+	theme: {},
+	plugins: []
 }
