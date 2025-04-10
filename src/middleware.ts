4d31f9968f5857693b8b536726e91e5e07e6c089
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-04-10 13:22:31 +0200

    update localization setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,7 @@
 import createMiddleware from "next-intl/middleware"
-// import {routing} from './i18n/routing';
+import { routing } from "./i18n/routing"
 
-export default createMiddleware({
-	locales: ["en", "ar", "fr", "es"],
-	localePrefix: "as-needed",
-	defaultLocale: "en",
-	localeDetection: true
-})
+export default createMiddleware(routing)
 
 export const config = {
 	// Match all pathnames except for
