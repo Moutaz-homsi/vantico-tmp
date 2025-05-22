3b70271a337efa0a1138241af91fb5b84452e3e6
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-22 12:02:21 +0200

    implement cta section

--- src/components/page-builder/blocks/renamelater.tsx
+++ src/components/page-builder/blocks/renamelater.tsx
@@ -1,17 +1,38 @@
 import { Button } from "@/components/ui"
 import { ChevronRight } from "lucide-react"
+import Image from "@/components/ui/image"
 import React from "react"
 
 export default function Renamelater() {
 	return (
-		<div>
-			<h6>Ready to transform your digital experience?</h6>
+		<section className="relative w-full min-h-[400px] flex items-center justify-start py-40 overflow-hidden">
+			{/* Background image on the right */}
+			<div className="absolute inset-0 pointer-events-none select-none">
+				<div className="hidden md:block absolute right-0 top-0 h-full w-1/2 z-0">
+					<Image
+						src="/bg-logo.png"
+						alt="Background logo"
+						isFill
+						styles={{}}
+						quality={80}
+						sizes="50vw"
+						fetchPriority="auto"
+						className="object-contain object-right h-full w-full opacity-80"
+					/>
+				</div>
+			</div>
 
-			<Button>
-				Schedule a free consultation <ChevronRight />
-			</Button>
+			<div className="relative z-10 flex flex-col items-start max-w-7xl mx-auto  ">
+				<h2 className="text-4xl md:text-6xl font-secondary tracking-tighter mb-12 uppercase text-black text-left">
+					Ready to transform
+					<br className="hidden md:block" />
+					your digital experience?
+				</h2>
 
-			{/* TODO background image */}
-		</div>
+				<Button variant="dark">
+					Schedule a free consultation <ChevronRight className="ml-2" size={24} />
+				</Button>
+			</div>
+		</section>
 	)
 }
