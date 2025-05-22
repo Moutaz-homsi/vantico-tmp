bb16a062bf374d4f02487b994a13fa31d121ff4a
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-22 16:08:48 +0200

    implement hover effect

--- src/components/page-builder/blocks/MeetTheTeam.tsx
+++ src/components/page-builder/blocks/MeetTheTeam.tsx
@@ -3,7 +3,8 @@ import React, { useState } from "react"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { ArrowRight, UserRound } from "lucide-react"
+import { ArrowRight, ChevronRight, UserRound } from "lucide-react"
+import Link from "next/link"
 
 interface MeetTheTeamProps {
 	members: []
@@ -29,9 +30,20 @@ const MeetTheTeam = ({ members, onReadMoreClick }: MeetTheTeamProps) => {
 
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 					{members.map((member) => (
-						<div key={member.id} className={`flex flex-col ${expandedMember === member.id ? "lg:col-span-2" : ""}`}>
-							<div className="h-80 mb-4 relative overflow-hidden">
+						<div key={member.id} className="flex flex-col group transition-all duration-300 relative bg-transparent">
+							<div className="h-80 mb-4 relative overflow-hidden ">
 								<img src={member.image} alt={member.name} className="w-full h-full object-cover grayscale" />
+								{/* Overlay for bio on hover */}
+
+								<div className="absolute inset-0 bg-black bg-opacity-95 flex flex-col justify-between opacity-0  group-hover:opacity-100 transition-opacity duration-300 p-8">
+									<p className="text-xl !leading-[140%] text-white mb-8 line-clamp-6">{member.bio}</p>
+									<Link
+										href={`/team/${member.id}`}
+										className="text-xl text-white hover:text-primary self-end flex items-center gap-2 transition-colors"
+									>
+										Read more <ChevronRight className="ml-3" size={18} />
+									</Link>
+								</div>
 							</div>
 
 							<h3 className="text-3xl mb-1">{member.name}</h3>
