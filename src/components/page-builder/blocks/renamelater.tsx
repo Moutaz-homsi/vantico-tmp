517b960b20e91113d2cf99097aa8752b12d1f404
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-25 17:42:43 +0200

    update ready section

--- src/components/page-builder/blocks/renamelater.tsx
+++ src/components/page-builder/blocks/renamelater.tsx
@@ -5,10 +5,7 @@ import React from "react"
 
 export default function Renamelater() {
 	return (
-		<section
-			id="renamelater"
-			className="relative w-full min-h-[400px] flex items-center justify-start py-40 overflow-hidden"
-		>
+		<section id="renamelater" className="relative w-full flex items-center justify-start p-30 overflow-hidden">
 			{/* Background image on the right */}
 			<div className="absolute inset-0 pointer-events-none select-none">
 				<div className="hidden md:block absolute right-0 top-0 h-full w-1/2 z-0">
@@ -25,10 +22,10 @@ export default function Renamelater() {
 				</div>
 			</div>
 
-			<div className="relative z-10 flex flex-col items-start max-w-7xl mx-auto  ">
-				<h2 className="text-4xl md:text-6xl font-secondary tracking-tighter mb-12 uppercase text-black text-left">
+			<div className="relative z-10 flex flex-col gap-14 items-start max-w-6xl mx-auto  ">
+				<h2 className="text-[56px] leading-[100%] font-secondary tracking-[-4%]  text-black text-left">
 					Ready to transform
-					<br className="hidden md:block" />
+					<br />
 					your digital experience?
 				</h2>
 
