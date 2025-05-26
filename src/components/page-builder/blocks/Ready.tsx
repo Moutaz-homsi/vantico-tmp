d64d16e7744e79392b30f1a75739d38d6c275f56
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-26 18:35:15 +0200

    handle responsive

--- src/components/page-builder/blocks/Ready.tsx
+++ src/components/page-builder/blocks/Ready.tsx
@@ -2,32 +2,30 @@ import { Button } from "@/components/ui"
 import { ChevronRight } from "lucide-react"
 import Image from "@/components/ui/image"
 import React from "react"
+import Title from "@/components/ui/title"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 export default function Ready() {
 	return (
-		<section id="ready" className="relative w-full flex items-center justify-start p-30 overflow-hidden">
+		<section id="ready" className="relative w-full flex items-center justify-start overflow-hidden">
 			{/* Background image on the right */}
-			<div className="absolute inset-0 pointer-events-none select-none">
-				<div className="hidden md:block absolute right-0 top-0 h-full w-1/2 z-0">
-					<Image
-						src="/bg-logo.png"
-						alt="Background logo"
-						isFill
-						styles={{}}
-						quality={80}
-						sizes="50vw"
-						fetchPriority="auto"
-						className="object-contain object-right h-full w-full opacity-80"
-					/>
-				</div>
+			<div className="absolute right-0 top-0 h-full w-3/4 md:w-1/3 z-0">
+				<Image
+					src="/bg-logo.png"
+					alt="Background logo"
+					isFill
+					quality={80}
+					fetchPriority="auto"
+					className=" object-right h-full w-full opacity-80"
+				/>
 			</div>
 
-			<div className="relative z-10 flex flex-col gap-14 items-start max-w-6xl mx-auto  ">
-				<h2 className="text-[56px] leading-[100%] font-secondary tracking-[-4%]  text-black text-left">
+			<div className=" z-10 flex flex-col gap-14 items-start max-w-6xl mx-auto  px-8 py-16 md:p-30 ">
+				<Title className="">
 					Ready to transform
 					<br />
 					your digital experience?
-				</h2>
+				</Title>
 
 				<Button variant="dark">
 					Schedule a free consultation <ChevronRight className="ml-2" size={24} />
