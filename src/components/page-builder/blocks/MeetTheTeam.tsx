a0f61f78185dfefdbbde2850f35e747af768d2e0
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-26 12:44:12 +0200

    improve team section

--- src/components/page-builder/blocks/MeetTheTeam.tsx
+++ src/components/page-builder/blocks/MeetTheTeam.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { ArrowRight, ChevronRight, UserRound } from "lucide-react"
 import Link from "next/link"
+import Title from "@/components/ui/title"
 
 interface MeetTheTeamProps {
 	members: []
@@ -22,16 +23,15 @@ const MeetTheTeam = ({ members, onReadMoreClick }: MeetTheTeamProps) => {
 	return (
 		<section id="team" className="bg-black text-white py-40 px-4">
 			<div className="container mx-auto max-w-6xl">
-				<h2 className="text-5xl lg:text-6xl font-secondary mb-4 tracking-tighter">MEET THE TEAM</h2>
-				<p className="text-gray-300 mb-12 max-w-2xl">
-					Our team of experienced professionals brings diverse expertise in real estate, healthcare, and business
-					development.
+				<Title className="mb-4 text-white">Meet the team</Title>
+				<p className="text-[#ABABAB] text-2xl leading-[160%] mt-6 max-w-2xl">
+					Lorem ipsum dolor sit amet consectetur adisplicing elit
 				</p>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-y-11">
 					{members.map((member) => (
 						<div key={member.id} className="flex flex-col group transition-all duration-300 relative bg-transparent">
-							<div className="h-80 mb-4 relative overflow-hidden ">
+							<div className="h-100 mb-10 relative overflow-hidden ">
 								<img src={member.image} alt={member.name} className="w-full h-full object-cover grayscale" />
 								{/* Overlay for bio on hover */}
 
@@ -46,8 +46,8 @@ const MeetTheTeam = ({ members, onReadMoreClick }: MeetTheTeamProps) => {
 								</div>
 							</div>
 
-							<h3 className="text-3xl mb-1">{member.name}</h3>
-							<p className="text-neutral-400 mb-2">{member.title}</p>
+							<h3 className="text-3xl mb-2">{member.name}</h3>
+							<p className="text-[#ABABAB] pr-4">{member.title}</p>
 						</div>
 					))}
 				</div>
