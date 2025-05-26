27896486d27da853e6d5406dd15349450438a135
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-26 19:21:29 +0200

    make FAQ section responsive

--- src/components/page-builder/blocks/FAQ.tsx
+++ src/components/page-builder/blocks/FAQ.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 import { ChevronDown, ChevronUp, ChevronRight } from "lucide-react"
 import { cn } from "@/utils"
 import { Button } from "@/components/ui"
+import Title from "@/components/ui/title"
 
 export type FAQItem = {
 	question: string
@@ -24,21 +25,21 @@ const FAQ: React.FC<FAQProps> = ({ title = "FAQs", items, ctaText = "Ask your ow
 	}
 
 	return (
-		<section id="faq" className="w-full max-w-3xl mx-auto py-40 px-4">
-			<h2 className="text-5xl font-secondary  tracking-tighter text-white mb-12">{title}</h2>
+		<section id="faq" className="w-full max-w-3xl mx-auto py-10 md:py-40 px-4">
+			<Title className="text-white mb-12">{title}</Title>
 
-			<div className="space-y-6 my-20">
+			<div className="space-y-6 my-10 md:my-20">
 				{/* Questions */}
 				{items.map((item, index) => (
 					<div
 						key={index}
-						className="bg-[#1E1E1E] p-10 hover:bg-[#2a2a2a]  rounded-md overflow-hidden transition-all duration-300"
+						className="bg-[#1E1E1E] p-6 md:p-10 hover:bg-[#2a2a2a]  rounded-md overflow-hidden transition-all duration-300"
 					>
 						<button
 							onClick={() => toggleQuestion(index)}
 							className="w-full flex justify-between items-center text-left text-white  transition-colors"
 						>
-							<h3 className="text-2xl font-semibold">{item.question}</h3>
+							<h3 className="text-lg md:text-2xl font-semibold">{item.question}</h3>
 							{openIndex === index ? (
 								<ChevronUp className="flex-shrink-0 ml-4" />
 							) : (
@@ -52,14 +53,14 @@ const FAQ: React.FC<FAQProps> = ({ title = "FAQs", items, ctaText = "Ask your ow
 								openIndex === index ? "max-h-96 pb-6 mt-6" : "max-h-0"
 							)}
 						>
-							<p className="leading-relaxed text-lg">{item.answer}</p>
+							<p className="leading-relaxed text-md lg:text-lg">{item.answer}</p>
 						</div>
 					</div>
 				))}
 			</div>
 
 			{ctaText && (
-				<div className="flex justify-between items-center">
+				<div className="flex flex-col md:flex-row gap-4 md:justify-between items-center">
 					<p className="text-2xl text-white">Couldn't find an answer?</p>
 					<Button className="font-semibold" onClick={onCtaClick}>
 						{ctaText} <ChevronRight className="ml-2" />
