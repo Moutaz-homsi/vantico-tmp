1bc4528959aff8a84eb9117b42d6256d63b595d6
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-26 23:58:40 +0200

    modify spacing

--- src/components/page-builder/blocks/MeetTheTeam.tsx
+++ src/components/page-builder/blocks/MeetTheTeam.tsx
@@ -21,21 +21,21 @@ const MeetTheTeam = ({ members, onReadMoreClick }: MeetTheTeamProps) => {
 	}
 
 	return (
-		<section id="team" className="bg-black text-white py-40 px-4">
+		<section id="team" className="bg-black text-white py-10 md:py-40 px-4">
 			<div className="container mx-auto max-w-7xl">
-				<Title className="mb-4 text-white font-secondary">Meet the team</Title>
-				<p className="text-neutral-400 text-2xl leading-[160%] mt-6 max-w-2xl">
+				<Title className="text-white font-secondary">Meet the team</Title>
+				<p className="text-neutral-400 text-lg md:text-2xl leading-[160%] mt-2 md:mt-6 max-w-2xl">
 					Lorem ipsum dolor sit amet consectetur adisplicing elit
 				</p>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-y-11">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  mt-8 md:mt-20 gap-4 md:gap-y-11">
 					{members.map((member) => (
 						<div
 							key={member.id}
 							className="flex flex-col group transition-all duration-300 relative bg-transparent"
 							onClick={() => handleCardClick(member.id)}
 						>
-							<div className="h-100 mb-10 relative overflow-hidden ">
+							<div className="h-100 md:mb-10 relative overflow-hidden ">
 								{/* Use custom Image component as per your guide */}
 								<div className="relative w-full h-[340px]">
 									<Image src={member.image} alt={member.name} isFill className="object-cover grayscale" />
@@ -51,7 +51,7 @@ const MeetTheTeam = ({ members, onReadMoreClick }: MeetTheTeamProps) => {
                                         group-hover:pointer-events-auto
                                     `}
 								>
-									<p className="text-xl !leading-[140%] text-white mb-8 line-clamp-6">{member.bio}</p>
+									<p className="text-lg md:text-xl !leading-[140%] text-white mb-8 line-clamp-6">{member.bio}</p>
 									<Link
 										href={`/team/${member.id}`}
 										className="text-xl text-white hover:text-primary self-end flex items-center gap-2 transition-colors"
