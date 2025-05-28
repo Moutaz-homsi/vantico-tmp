e2c5686cb7351c86bba562cd7944baf36234dc4d
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-05-28 11:44:53 +0200

    improve roi calculator

--- src/components/page-builder/blocks/ROICalculator.tsx
+++ src/components/page-builder/blocks/ROICalculator.tsx
@@ -14,17 +14,17 @@ export default function ROICalculator() {
 	const annualROI = investmentPeriod > 0 ? roi / (investmentPeriod / 12) : 0
 
 	return (
-		<section id="roi" className="bg-black px-4 md:px-30  py-10 md:py-40">
-			<div className="bg-[#1E1E1E] text-white rounded-md p-8 md:p-20">
+		<section id="roi" className="bg-black  px-8 md:px-0  py-10 md:py-40">
+			<div className="bg-[#1E1E1E] lg:max-w-7xl mx-auto text-white rounded-md p-8 md:p-20">
 				{/* title */}
-				<Title className="text-white text-center mb-12 ">ROI Calculator</Title>
+				<Title className="text-white text-center ">ROI Calculator</Title>
 				{/* inputs */}
-				<div className="grid md:grid-cols-3 gap-6 mb-12">
+				<div className="grid lg:grid-cols-3 gap-6 mt-10 md:mt-20">
 					{/* first input: invested */}
-					<div>
-						<label className="block text-sm font-light mb-3 text-neutral-300">Amount invested</label>
+					<div className="space-y-6">
+						<label className="block text-sm font-light text-[#ABABAB]">Amount invested</label>
 						<div className="flex items-center bg-[#161616] rounded-sm px-8 py-6">
-							<span className="text-neutral-500 text-xl mr-2">$</span>
+							<span className="text-[#535353] text-xl mr-2">$</span>
 							<input
 								type="number"
 								value={amountInvested}
@@ -35,10 +35,10 @@ export default function ROICalculator() {
 						</div>
 					</div>
 					{/* second input: returned */}
-					<div>
-						<label className="block text-sm font-light mb-3 text-neutral-300">Amount returned</label>
+					<div className="space-y-6">
+						<label className="block text-sm font-light  text-[#ABABAB]">Amount returned</label>
 						<div className="flex items-center bg-[#161616] rounded-sm px-8 py-6">
-							<span className="text-neutral-500 text-xl mr-2">$</span>
+							<span className="text-[#535353] text-xl mr-2">$</span>
 							<input
 								type="number"
 								value={amountReturned}
@@ -49,8 +49,8 @@ export default function ROICalculator() {
 						</div>
 					</div>
 					{/* third input: months */}
-					<div>
-						<label className="block text-sm font-light mb-3 text-neutral-300">Investment period (in months)</label>
+					<div className="space-y-6">
+						<label className="block text-sm font-light text-[#ABABAB]">Investment period (in months)</label>
 						<div className="flex items-center justify-between bg-[#161616] rounded-sm px-8 py-6">
 							<input
 								type="number"
@@ -59,27 +59,36 @@ export default function ROICalculator() {
 								className="w-full bg-transparent text-white text-2xl  outline-none  pr-2"
 								style={{ fontFamily: "Montserrat, Arial, sans-serif", letterSpacing: 1 }}
 							/>
-							<span className="text-neutral-500 ml-3  text-xl">Months</span>
+							<span className="text-[#535353] ml-3  text-xl">Months</span>
 						</div>
 					</div>
 				</div>
 				{/* results */}
-				<div className="flex flex-col gap-12 text-2xl font-light">
-					<div className="border-b border-neutral-700 py-6 flex items-center justify-between">
-						<span className="text-neutral-200 text-[1.35rem]">Total Gain on Investment</span>
-						<span className="text-white font-normal text-4xl" style={{ fontFamily: "Montserrat, Arial, sans-serif" }}>
+				<div className="flex flex-col gap-8 md:gap-20 text-2xl mt-8 md:mt-16 font-light">
+					<div className="border-b border-neutral-700 py-6 flex items-center gap-2 justify-between">
+						<span className="text-[#ABABAB] text-xl md:text-2xl lg:text-3xl">Total Gain on Investment</span>
+						<span
+							className="text-white font-medium text-3xl lg:text-5xl"
+							style={{ fontFamily: "Montserrat, Arial, sans-serif" }}
+						>
 							{addCommas(totalGain)}
 						</span>
 					</div>
-					<div className="border-b border-neutral-700 py-6 flex items-center justify-between">
-						<span className="text-neutral-200 text-[1.35rem]">Return on Investment (ROI)</span>
-						<span className="text-white font-normal text-4xl" style={{ fontFamily: "Montserrat, Arial, sans-serif" }}>
+					<div className="border-b border-neutral-700 py-6 flex items-center gap-2 justify-between">
+						<span className="text-[#ABABAB] text-xl md:text-2xl lg:text-3xl">Return on Investment (ROI)</span>
+						<span
+							className="text-white font-medium text-3xl lg:text-5xl"
+							style={{ fontFamily: "Montserrat, Arial, sans-serif" }}
+						>
 							{addCommas(roi.toFixed(2))}%
 						</span>
 					</div>
-					<div className="border-b border-neutral-700 py-6 flex items-center justify-between">
-						<span className="text-neutral-200 text-[1.35rem]">Simple Annual (ROI)</span>
-						<span className="text-white font-normal text-4xl" style={{ fontFamily: "Montserrat, Arial, sans-serif" }}>
+					<div className="border-b border-neutral-700 py-6 flex items-center gap-2 justify-between">
+						<span className="text-[#ABABAB] text-xl md:text-2xl lg:text-3xl">Simple Annual (ROI)</span>
+						<span
+							className="text-white font-medium text-3xl  lg:text-5xl"
+							style={{ fontFamily: "Montserrat, Arial, sans-serif" }}
+						>
 							{addCommas(annualROI.toFixed(1))}%
 						</span>
 					</div>
