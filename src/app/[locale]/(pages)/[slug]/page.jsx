ffcfa630e8e864d46fcc73efb989d419a7767283
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-06-26 08:10:49 +0200

    update page rendering options

--- src/app/[locale]/(pages)/[slug]/page.jsx
+++ src/app/[locale]/(pages)/[slug]/page.jsx
@@ -1,2 +1,2 @@
-// export { default, generateStaticParams, revalidate } from "./dynamic-slug-page"
-export { default } from "./dynamic-slug-page"
+export { default, generateStaticParams, revalidate } from "./dynamic-slug-page"
+// export { default } from "./dynamic-slug-page"
