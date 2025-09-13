09d4697dcd6011fb68184bdeb1b6a9b6c66365e7
ibrahim hajjar <ibraheemhajjar91@gmail.com>
2025-09-13 15:35:25 +0300

    update hero video and globe video components

--- src/components/page-builder/blocks/globe-section.tsx
+++ src/components/page-builder/blocks/globe-section.tsx
@@ -1,28 +1,21 @@
-import { Image } from "@/components/ui"
-import VideoSection from "@/components/ui/video-section"
+import VideoBackground from "./VideoBackground"
 
 export default function GlobeSection() {
 	return (
-		<VideoSection
-			video_url={"https://vimeo.com/1118114913"}
-			id="globe-video"
-			className="px-6 md:px-8 lg:px-16"
-			showVideoOverlay={false}
+		<VideoBackground
+			videoSrc={
+				"https://cdn.jsdelivr.net/gh/moutazdev/my-website-videos@main/366591_Sun_Light_24_Hours_Circling_Stars_By_Frame_Stock_Footage_Artlist_HD_2.mp4"
+			}
+			className="h-[450px] lg:h-[600px] xl:h-[750px] 2xl:h-[900px] px-6 md:px-8 lg:px-16"
+			childrenContainerClassName="h-full flex items-center justify-start max-w-7xl mx-auto"
 		>
-			<div className="w-full max-w-7xl mx-auto h-full flex items-center justify-start">
-				<div className="relative z-10 pt-0 pb-0 flex flex-col gap-y-10">
-					<h1 className="text-4xl md:text-[4rem] font-medium font-secondary leading-[100%] text-white text-start">
-						Trust
-					</h1>
-					<h1 className="text-4xl md:text-[4rem] font-medium font-secondary leading-[100%] text-white text-start">
-						<span className="cd-words-wrapper">Alignment</span>
-					</h1>
-					<h1 className="text-4xl md:text-[4rem] font-medium font-secondary leading-[100%] text-white text-start">
-						<span className="cd-words-wrapper">Reach</span>
-					</h1>
-					{/* <p className="text-lg md:text-2xl text-[#ABABAB] mt-6 mb-10 max-w-3xl mx-auto">{subtitle}</p> */}
+			<div className="flex items-center justify-start">
+				<div className="relative z-10 pt-0 pb-0 flex flex-col gap-y-8 text-4xl md:text-[3rem] lg:text-[3.5rem] xl:text-[4rem] font-medium font-secondary text-white text-start">
+					<h2>Trust</h2>
+					<h2>Alignment</h2>
+					<h2>Reach</h2>
 				</div>
 			</div>
-		</VideoSection>
+		</VideoBackground>
 	)
 }
